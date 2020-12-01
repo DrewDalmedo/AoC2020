73643b1173cb972b5069c824a67c85454cc03234
DrewDalmedo <andrewdalmedo@gmail.com>
2020-12-01 10:20:03 -0500

    Add modified findNums function and print result

--- 1/p2/solution.js
+++ 1/p2/solution.js
@@ -5,3 +5,30 @@ Using the above example again, the three entries that sum to 2020 are 979, 366, 
 
 In your expense report, what is the product of the three entries that sum to 2020?
 */
+
+// reuse getInput function from p1 solution
+// bug: importing the function runs the entire p1 solution itself, not ideal.
+import { getInput } from '../p1/solution.js'
+
+// time complexity: O(n^3) (VERY terrible!!!!!)
+const findNums = (arr, target) => {
+  for (let x = 0; x < arr.length; x++) 
+    for (let y = x + 1; y < arr.length; y++) 
+      for (let z = y + 1; z < arr.length; z++)
+        if (arr[x] + arr[y] + arr[z] === target)
+          return [ arr[x], arr[y], arr[z] ]
+    
+  return null 
+}
+
+
+let input = getInput('./input.txt')
+
+let targetNums = findNums(input, 2020)
+
+if (targetNums !== null) {
+  console.log( targetNums[0] * targetNums[1] * targetNums[2] )
+}
+else {
+  console.log( 'no solution found' )
+}
